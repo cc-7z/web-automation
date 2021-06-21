f5b53abbd7d840719d9d1748746110e8dbfbfdc1
leon <leoneffortless@163.com>
2021-06-21 14:39:10 +0800

    init

---
+++ src/main/java/com/godleon/auto/AutoApplication.java
@@ -0,0 +1,18 @@
+package com.godleon.auto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * web-automation
+ * @author leon
+ * @date 2021-06-21
+ */
+@SpringBootApplication
+public class AutoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/godleon/auto/AutoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.godleon.auto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
